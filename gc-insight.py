dbef5b2f8a0bf8d4ab0ef5c690d4d65ef4ddf7f6
Carroll, Brian <bcarroll@arifleet.com>
2021-08-10 14:46:38 -0400

    first push

---
+++ gc-insight.py
@@ -0,0 +1,65 @@
+import json
+import logging
+import yaml
+import time
+import logging
+from pyaml_env import parse_config
+from argparse import ArgumentParser
+from guardicore.centra import CentraAPI
+
+
+def load_config(path="config.yml"):
+    """
+    Loads the configuration file for the application
+    and returns a configuration object for consumption in
+    other areas
+    """
+    config_error = False
+    config = parse_config(path)
+
+    return config
+
+
+if __name__ == "__main__":
+    # Set the logging format
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    
+    # Parse script parameters
+    parser = ArgumentParser()
+    parser.add_argument('--config', help="The path to the configuration file", default="config.yml", required=False)
+    parser.add_argument('--query', help="The OS Query you want to run", required=False)
+    parser.add_argument('--query-id', help="Fetch the results from a previous query ID", required=False)
+    args = parser.parse_args()
+
+    # Load the configuration
+    config = load_config(path=args.config)
+
+    # Authenticate to Guardicore
+    logging.info("Authenticating to Guardicore")
+    centra = CentraAPI(management_url=config['guardicore']['management_url'])
+    
+    try:
+        centra.authenticate(username=config['guardicore']['username'], password=config['guardicore']['password'])
+    except Exception as e:
+        logging.error(e)
+        exit(1)
+
+    # Initiate the query
+    query_id = centra.insight_query("run", args.query, agent_filter={"os": ["Windows"]})
+
+    # Make sure the query executed successfully
+    if query_id:
+        query_status = None
+
+        # Wait for the query to finish
+        while query_status != "DONE":
+            query_status = centra.insight_query_info(query_id, status_only=True)
+            if query_status == "DONE":
+                break
+
+            logging.info(f"Waiting for query {query_id} to finish...current status {query_status}")            
+            time.sleep(10)
+        
+        query_results = centra.insight_query_results(query_id)
+        print(json.dumps(query_results, indent=4))
